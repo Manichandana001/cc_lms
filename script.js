1697884d9db5d07aebb1c71bab1b2294e04f5c18
Manichandana001 <manichandanaadire@gmail.com>
2025-04-02 21:24:29 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const meetingButtons = document.querySelectorAll(".meeting-btn");
-    
-    meetingButtons.forEach(button => {
-        button.addEventListener("click", function () {
-            alert("Redirecting to Meeting Link...");
-        });
-    });
-});
